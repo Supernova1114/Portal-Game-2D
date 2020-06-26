881edcaeac0a165a565a4ddc09b6b48544bed214
Supernova1114 <supernovawarriors128@gmail.com>
2020-06-26 10:50:50 -0700

    Started Project

---
+++ Portal-Game-2D/Assets/Scripts/TeleportationHandlerBlue.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportationHandlerBlue : MonoBehaviour
+{
+    private GameObject orangePortal;
+
+    public bool isInProgress = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        orangePortal = GameObject.FindGameObjectWithTag("orangePortal");
+        //GameObject.Find("Companion Cube").GetComponent<Rigidbody2D>().velocity = new Vector2( 3, 10 );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector2 vect;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isInProgress = true;
+
+        if (!(orangePortal.GetComponent<TeleportationHandlerOrange>().isInProgress))
+        {
+            vect = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
+
+            collision.gameObject.transform.position = orangePortal.transform.position;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        orangePortal.GetComponent<TeleportationHandlerOrange>().isInProgress = false;
+    }
+
+
+}
---
+++ Portal-Game-2D/Assets/Scripts/TeleportationHandlerOrange.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportationHandlerOrange : MonoBehaviour
+{
+    private GameObject bluePortal;
+
+    public bool isInProgress = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bluePortal = GameObject.FindGameObjectWithTag("bluePortal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    Vector2 vect;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isInProgress = true;
+
+        if ( !(bluePortal.GetComponent<TeleportationHandlerBlue>().isInProgress) )
+        {
+            vect = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
+
+            collision.gameObject.transform.position = bluePortal.transform.position;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        bluePortal.GetComponent<TeleportationHandlerBlue>().isInProgress = false;
+    }
+
+}
