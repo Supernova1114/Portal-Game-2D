c64e757411773c231e627a30bd6566b3571bc7b2
Supernova1114 <supernovawarriors128@gmail.com>
2020-07-01 14:25:48 -0700

    Portal placing almost done

---
+++ Portal-Game-2D/Assets/Scripts/Pointer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    Camera cam;
+
+    Vector3 point;
+    public static Vector3 mousePos;
+
+    float angle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        point = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        mousePos = point;
+
+        point -= transform.position;
+
+        point = point.normalized;
+
+
+        angle = Mathf.Atan2(point.y, point.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+        
+        
+    }
+}
---
+++ Portal-Game-2D/Assets/Scripts/PortalPlacer.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalPlacer : MonoBehaviour
+{
+
+    public GameObject bluePortal;
+    public GameObject orangePortal;
+
+    //public Rigidbody2D body;
+
+    RaycastHit2D cast;
+
+    Vector2 mousePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if ( Input.GetMouseButtonDown(0))
+        {
+            //print("heya");
+            PlaceBlue();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            PlaceOrange();
+        }
+
+    }
+
+    void PlaceBlue()
+    {
+        if ( GameObject.FindWithTag("bluePortal") != null)
+        {
+            //print("placed");
+            mousePos = (Vector2)Pointer.mousePos;
+
+            //body.velocity = (mousePos - (Vector2)transform.position).normalized * 5;
+
+            cast = Physics2D.Raycast((Vector2)transform.position, (mousePos - (Vector2)transform.position).normalized);
+
+
+            if (cast.collider != null)
+            {
+                bluePortal.transform.position = cast.point;
+
+                Vector2 bNormal = cast.normal;
+
+                float angle = Mathf.Atan2(bNormal.y, bNormal.x) * Mathf.Rad2Deg;
+
+                bluePortal.transform.rotation = Quaternion.Euler(0, 0, angle - 90);
+
+            }
+            
+        }
+    }
+
+    void PlaceOrange()
+    {
+        if (GameObject.FindWithTag("orangePortal") != null)
+        {
+            //print("placed");
+            mousePos = (Vector2)Pointer.mousePos;
+
+            //body.velocity = (mousePos - (Vector2)transform.position).normalized * 5;
+
+            cast = Physics2D.Raycast((Vector2)transform.position, (mousePos - (Vector2)transform.position).normalized);
+
+
+            if (cast.collider != null)
+            {
+                orangePortal.transform.position = cast.point;
+
+                Vector2 bNormal = cast.normal;
+
+                float angle = Mathf.Atan2(bNormal.y, bNormal.x) * Mathf.Rad2Deg;
+
+                orangePortal.transform.rotation = Quaternion.Euler(0, 0, angle - 90);
+
+            }
+
+        }
+    }
+
+
+
+
+
+}
