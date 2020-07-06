22bdf16c34662b370529b77cc6c188a3233d6f68
Supernova1114 <supernovawarriors128@gmail.com>
2020-07-05 22:11:02 -0700

    Added Velocity countdown

--- Portal-Game-2D/Assets/Scripts/HealthHandler.cs
+++ Portal-Game-2D/Assets/Scripts/HealthHandler.cs
@@ -12,17 +12,25 @@ public class HealthHandler : MonoBehaviour
     public Text text;
 
     private bool isAlive = true;
+    public Rigidbody2D body;
+
+    public Text text2;
+
+    private bool flag = true;
+
+    private int count = 4;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(VelChecker());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -45,5 +53,39 @@ public class HealthHandler : MonoBehaviour
             }
         }
     }
+
+    IEnumerator VelChecker()
+    {
+
+        yield return new WaitForSeconds(3);
+
+        while ( flag )
+        {
+            yield return new WaitForSeconds(0.0f);
+            if (  Mathf.Abs(body.velocity.x) < 0.7f && Mathf.Abs(body.velocity.y) < 0.7f )
+            {
+               
+                count--;
+                text2.text = count.ToString();
+                yield return new WaitForSeconds(1f);
+            }
+            else
+            {
+                text2.text = "";
+                count = 4;
+            }
+
+            if (count == 0)
+            {
+                flag = false;
+            }
+
+        }
+
+        yield return new WaitForSeconds(0.5f);
+        health = 0;
+        
+    }
+
 
 }
